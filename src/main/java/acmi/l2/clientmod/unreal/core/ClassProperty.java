b58278b06a5ad88d26a1a1a1712cca5b9e9abf31
Aleksei Sazonov <s.aleksey@russianit.ru>
2016-05-12 17:55:49 +0400

    Fix #3

--- src/main/java/acmi/l2/clientmod/unreal/core/ClassProperty.java
+++ src/main/java/acmi/l2/clientmod/unreal/core/ClassProperty.java
@@ -1,4 +1,8 @@
 package acmi.l2.clientmod.unreal.core;
 
+import acmi.l2.clientmod.unreal.annotation.ObjectRef;
+
 public class ClassProperty extends ObjectProperty {
+    @ObjectRef
+    public acmi.l2.clientmod.unreal.core.Class clazz;
 }
