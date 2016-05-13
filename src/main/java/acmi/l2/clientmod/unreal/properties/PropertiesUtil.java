e7093c2738d074a6c5d2fd8ba0319b4bc2269309
Aleksei Sazonov <s.aleksey@russianit.ru>
2016-05-13 18:01:43 +0400

    properties write

--- src/main/java/acmi/l2/clientmod/unreal/properties/PropertiesUtil.java
+++ src/main/java/acmi/l2/clientmod/unreal/properties/PropertiesUtil.java
@@ -21,16 +21,14 @@
  */
 package acmi.l2.clientmod.unreal.properties;
 
-import acmi.l2.clientmod.io.DataInput;
-import acmi.l2.clientmod.io.ObjectInput;
-import acmi.l2.clientmod.io.ObjectInputStream;
-import acmi.l2.clientmod.io.UnrealPackage;
+import acmi.l2.clientmod.io.*;
 import acmi.l2.clientmod.unreal.UnrealException;
 import acmi.l2.clientmod.unreal.UnrealRuntimeContext;
 import acmi.l2.clientmod.unreal.UnrealSerializerFactory;
 import acmi.l2.clientmod.unreal.core.*;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.lang.Class;
@@ -101,60 +99,6 @@ public class PropertiesUtil {
         }
 
         return properties;
-    }
-
-    public static Type getPropertyType(int info) {
-        return Type.values()[info & 0b1111];
-    }
-
-    public static int getPropertySizeType(int info) {
-        return (info >> 4) & 0b111;
-    }
-
-    public static boolean isArray(int info) {
-        return info >> 7 != 0;
-    }
-
-    public static int readPropertySize(int sizeType, DataInput dataInput) throws IOException {
-        switch (sizeType) {
-            case 0:
-                return 1;
-            case 1:
-                return 2;
-            case 2:
-                return 4;
-            case 3:
-                return 12;
-            case 4:
-                return 16;
-            case 5:
-                return dataInput.readUnsignedByte();
-            case 6:
-                return dataInput.readUnsignedShort();
-            case 7:
-                return dataInput.readInt();
-            default:
-                throw new IllegalArgumentException(String.valueOf(sizeType));
-        }
-    }
-
-    public enum Type {
-        NONE,
-        BYTE,
-        INT,
-        BOOL,
-        FLOAT,
-        OBJECT,
-        NAME,
-        _DELEGATE,
-        _CLASS,
-        ARRAY,
-        STRUCT,
-        _VECTOR,
-        _ROTATOR,
-        STR,
-        _MAP,
-        _FIXED_ARRAY
     }
 
     public static Object read(ObjectInput<UnrealRuntimeContext> objBuffer, Type propertyType, boolean array, Property arrayInner, Struct struct) throws UncheckedIOException {
@@ -228,6 +172,196 @@ public class PropertiesUtil {
                 .collect(Collectors.toList());
     }
 
+    public static void writeProperties(ObjectOutput<UnrealRuntimeContext> output, List<L2Property> properties) {
+        UnrealPackage up = output.getContext().getUnrealPackage();
+
+        for (L2Property property : properties) {
+            Property template = property.getTemplate();
+
+            for (int i = 0; i < property.getSize(); i++) {
+                Object obj = property.getAt(i);
+                if (obj == null)
+                    continue;
+
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                ObjectOutput<UnrealRuntimeContext> objBuffer = new ObjectOutputStream<>(baos, output.getCharset(), 0, output.getSerializerFactory(), output.getContext());
+                write(objBuffer, template, obj);
+                byte[] bytes = baos.toByteArray();
+
+                Type type = getType(template);
+                int size = getPropertySize(bytes.length);
+                boolean array = (i > 0) || (type == Type.BOOL && ((Boolean) obj));
+                int info = (array ? 1 << 7 : 0) | (size << 4) | type.ordinal();
+
+                output.writeCompactInt(up.nameReference(template.entry.getObjectName().getName()));
+                output.writeByte(info);
+                if (type == Type.STRUCT)
+                    output.writeCompactInt(up.nameReference(((StructProperty) template).struct.entry.getObjectName().getName()));
+                switch (size) {
+                    case 5:
+                        output.writeByte(bytes.length);
+                        break;
+                    case 6:
+                        output.writeShort(bytes.length);
+                        break;
+                    case 7:
+                        output.writeInt(bytes.length);
+                        break;
+                }
+                if (i > 0)
+                    output.writeByte(i);
+                output.write(bytes);
+            }
+        }
+        output.writeCompactInt(up.nameReference("None"));
+    }
+
+    public static void write(ObjectOutput<UnrealRuntimeContext> objBuffer, Property template, Object obj) throws UncheckedIOException {
+        if (template instanceof ByteProperty) {
+            objBuffer.writeByte((Integer) obj);
+        } else if (template instanceof IntProperty) {
+            objBuffer.writeInt((Integer) obj);
+        } else if (template instanceof BoolProperty) {
+            //nothing
+        } else if (template instanceof FloatProperty) {
+            objBuffer.writeFloat((Float) obj);
+        } else if (template instanceof ObjectProperty) {
+            objBuffer.writeCompactInt((Integer) obj);
+        } else if (template instanceof NameProperty) {
+            objBuffer.writeCompactInt((Integer) obj);
+        } else if (template instanceof ArrayProperty) {
+            ArrayProperty arrayProperty = (ArrayProperty) template;
+
+            List<Object> arrayList = (List<Object>) obj;
+            objBuffer.writeCompactInt(arrayList.size());
+
+            Property arrayInner = arrayProperty.inner;
+            for (Object arrayObj : arrayList) {
+                write(objBuffer, arrayInner, arrayObj);
+            }
+        } else if (template instanceof StructProperty) {
+            StructProperty structProperty = (StructProperty) template;
+            writeStruct(objBuffer, structProperty.struct.getFullName(), (List<L2Property>) obj);
+        } else if (template instanceof StrProperty) {
+            objBuffer.writeLine((String) obj);
+        } else {
+            throw new UnsupportedOperationException(template.getClass().getSimpleName() + " serialization not implemented");
+        }
+    }
+
+    public static void writeStruct(ObjectOutput<UnrealRuntimeContext> objBuffer, String structName, List<L2Property> struct) throws UncheckedIOException {
+        switch (structName) {
+            case "Core.Object.Color":
+            case "Core.Object.Vector":
+            case "Core.Object.Rotator":
+                writeStructBin(objBuffer, struct, structName);
+                break;
+            default:
+                writeProperties(objBuffer, struct);
+        }
+    }
+
+    public static void writeStructBin(DataOutput objBuffer, List<L2Property> struct, String structName) throws UncheckedIOException {
+        switch (structName) {
+            case "Core.Object.Color":
+                for (int i = 0; i < 4; i++)
+                    objBuffer.writeByte((Integer) struct.get(i).getAt(0));
+                break;
+            case "Core.Object.Vector":
+                for (int i = 0; i < 3; i++)
+                    objBuffer.writeFloat((Float) struct.get(i).getAt(0));
+                break;
+            case "Core.Object.Rotator":
+                for (int i = 0; i < 3; i++)
+                    objBuffer.writeInt((Integer) struct.get(i).getAt(0));
+                break;
+            default:
+                throw new UnsupportedOperationException("not implemented");
+        }
+    }
+
+    public enum Type {
+        NONE(null),
+        BYTE(ByteProperty.class),
+        INT(IntProperty.class),
+        BOOL(BoolProperty.class),
+        FLOAT(FloatProperty.class),
+        OBJECT(ObjectProperty.class),
+        NAME(NameProperty.class),
+        _DELEGATE(DelegateProperty.class),
+        _CLASS(ClassProperty.class),
+        ARRAY(ArrayProperty.class),
+        STRUCT(StructProperty.class),
+        _VECTOR(StructProperty.class),
+        _ROTATOR(StructProperty.class),
+        STR(StrProperty.class),
+        _MAP(null),
+        _FIXED_ARRAY(null);
+
+        private Class<? extends Property> clazz;
+
+        Type(Class<? extends Property> clazz) {
+            this.clazz = clazz;
+        }
+    }
+
+    public static Type getPropertyType(int info) {
+        return Type.values()[info & 0b1111];
+    }
+
+    public static int getPropertySizeType(int info) {
+        return (info >> 4) & 0b111;
+    }
+
+    public static int getPropertySize(int size) {
+        switch (size) {
+            case 1:
+                return 0;
+            case 2:
+                return 1;
+            case 4:
+                return 2;
+            case 12:
+                return 3;
+            case 16:
+                return 4;
+            default:
+                if (size < 0x100)
+                    return 5;
+                else if (size < 0x10000)
+                    return 6;
+                else
+                    return 7;
+        }
+    }
+
+    public static boolean isArray(int info) {
+        return info >> 7 != 0;
+    }
+
+    public static int readPropertySize(int sizeType, DataInput dataInput) throws IOException {
+        switch (sizeType) {
+            case 0:
+                return 1;
+            case 1:
+                return 2;
+            case 2:
+                return 4;
+            case 3:
+                return 12;
+            case 4:
+                return 16;
+            case 5:
+                return dataInput.readUnsignedByte();
+            case 6:
+                return dataInput.readUnsignedShort();
+            case 7:
+                return dataInput.readInt();
+            default:
+                throw new IllegalArgumentException(String.valueOf(sizeType));
+        }
+    }
+
     public static L2Property getAt(List<L2Property> properties, String name) {
         return properties.stream()
                 .filter(p -> p.getName().equalsIgnoreCase((name)))
@@ -250,53 +384,13 @@ public class PropertiesUtil {
     }
 
     private static boolean match(Class<? extends Property> clazz, Type type) {
-        switch (type) {
-            case BYTE:
-                return ByteProperty.class.isAssignableFrom(clazz);
-            case INT:
-                return IntProperty.class.isAssignableFrom(clazz);
-            case BOOL:
-                return BoolProperty.class.isAssignableFrom(clazz);
-            case FLOAT:
-                return FloatProperty.class.isAssignableFrom(clazz);
-            case OBJECT:
-                return ObjectProperty.class.isAssignableFrom(clazz);
-            case NAME:
-                return NameProperty.class.isAssignableFrom(clazz);
-            case _DELEGATE:
-                return DelegateProperty.class.isAssignableFrom(clazz);
-            case _CLASS:
-                return ClassProperty.class.isAssignableFrom(clazz);
-            case ARRAY:
-                return ArrayProperty.class.isAssignableFrom(clazz);
-            case STRUCT:
-                return StructProperty.class.isAssignableFrom(clazz);
-            case STR:
-                return StrProperty.class.isAssignableFrom(clazz);
-            default:
-                throw new IllegalStateException();
-        }
+        return type.clazz.isAssignableFrom(clazz);
     }
 
     public static Type getType(Property property) {
-        if (property instanceof ByteProperty)
-            return Type.BYTE;
-        else if (property instanceof IntProperty)
-            return Type.INT;
-        else if (property instanceof BoolProperty)
-            return Type.BOOL;
-        else if (property instanceof FloatProperty)
-            return Type.FLOAT;
-        else if (property instanceof ObjectProperty)
-            return Type.OBJECT;
-        else if (property instanceof NameProperty)
-            return Type.NAME;
-        else if (property instanceof ArrayProperty)
-            return Type.ARRAY;
-        else if (property instanceof StructProperty)
-            return Type.STRUCT;
-        else if (property instanceof StrProperty)
-            return Type.STR;
+        for (Type type : Type.values())
+            if (match(property.getClass(), type))
+                return type;
         throw new IllegalStateException();
     }
 }
